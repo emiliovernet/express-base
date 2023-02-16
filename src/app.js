5f06304ebbfb7826f1334d4cdd544cd11f5b5676
emiliovernet <vernetemilio@gmail.com>
2023-02-16 01:19:12 -0300

    proyecto base express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mainRoutes = require('./routes/main')
 const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views')
